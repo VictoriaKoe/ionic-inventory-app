c9f72a5ab9217093982179a0c3e89702bbe8496a
Rui En Koe <ruienkoe@gmail.com>
2024-12-30 17:45:54 +0800

    create splashscreen to the app and change routing in app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,34 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  
+  // direct path to login
+  {
+    path: '',
+    redirectTo: 'loader',
+    pathMatch: 'full'
+  },
+
+  // splashscreen page
+  {
+    path: 'loader',
+    loadChildren: () => import('./pages/loader/loader.module').then( m => m.LoaderPageModule)
+  },
+
+  // home page
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   },
+
+  // login page
+
+
+
 ];
 
 @NgModule({
