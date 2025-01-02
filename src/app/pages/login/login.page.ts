2eab702655f5ae0bbb9ccba2b2869d604001b36c
Rui En Koe <ruienkoe@gmail.com>
2025-01-02 17:52:15 +0800

    complete login page ui and add button to navigate to different pages for future usages in login.page.ts file

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,23 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+// import forms
+import { Validators, FormBuilder, FormGroup, FormControl  } from '@angular/forms';
+// import router
+import { Router } from '@angular/router';
 import { IonInput } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
-  standalone: false
+  standalone: false,
 })
+
 export class LoginPage implements OnInit {
 
   // initialise default val
   username: String = '';
   password: String = '';
-  showPassword: Boolean = false;
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit() {}
 
+  // action after clicking sign in button
+  onSubmit(){
+    console.log(this.username, this.password);
+    // route to main page
+    this.router.navigate(['/loader']);
+  }
 
+  // route to register form
+  onRegister(){
+    this.router.navigate(['/loader']);
+  }
+
+  // route to forget password 
+  forgetPassword() {
+    this.router.navigate(['']);
+  }
 
 }
