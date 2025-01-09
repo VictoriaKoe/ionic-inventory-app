93df7b028d401e0eeb8c24eedb0dfffb7d3daef3
Rui En Koe <ruienkoe@gmail.com>
2025-01-09 17:58:53 +0800

    update user profile

--- src/app/pages/user-profile/user-profile.page.ts
+++ src/app/pages/user-profile/user-profile.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-profile',
@@ -8,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfilePage implements OnInit {
 
-  constructor() { }
+  // declare var
+
+  // dummy data
+  username: string = 'Vic'; 
+  email: string = 'vic@gmail.com';
+  password: string = '12345';
+  name: string = "Rui En";
+  pronoun: any = '-';
+  profilePhoto: any = "https://ionicframework.com/docs/img/demos/avatar.svg";
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+
+    // this.pronoun = [
+    //   "Male",
+    //   "Female"
+    // ];
+
+  }
+
+  // go to edit profile form 
+  // todo: create edit profile form
+  editProfile() {
+    this.router.navigate(['/editProfile']);
   }
 
 }
