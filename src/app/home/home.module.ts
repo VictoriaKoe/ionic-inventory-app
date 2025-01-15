2a4094e786a41e6b583995d3b5ae114cde7e447f
Rui En Koe <ruienkoe@gmail.com>
2025-01-15 17:59:20 +0800

    add nav bar to home page

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { NavigationBarComponent } from '../components/navigation-bar/navigation-bar.component';
 
 
 @NgModule({
@@ -12,7 +13,8 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    NavigationBarComponent
   ],
   declarations: [HomePage]
 })
