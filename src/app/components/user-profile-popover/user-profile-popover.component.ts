77b9cc92a75a3135b6aae9a8755d6c46fef981b2
Rui En Koe <ruienkoe@gmail.com>
2025-01-17 18:01:35 +0800

    cretae edit user profile modal

--- src/app/components/user-profile-popover/user-profile-popover.component.ts
+++ src/app/components/user-profile-popover/user-profile-popover.component.ts
@@ -1,14 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component, CUSTOM_ELEMENTS_SCHEMA, EventEmitter, Input, NO_ERRORS_SCHEMA, OnInit, Output } from '@angular/core';
-import { PopoverController } from '@ionic/angular';
-import { IonCol, IonGrid, IonRow } from '@ionic/angular/standalone';
+import { PopoverController, NavController } from '@ionic/angular';
+import { IonCol, IonGrid, IonRow, IonList } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-user-profile-popover',
   templateUrl: './user-profile-popover.component.html',
   styleUrls: ['./user-profile-popover.component.scss'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA], 
-  imports: [IonCol, IonGrid, IonRow, CommonModule],
+  imports: [IonCol, IonGrid, IonRow, CommonModule, IonList],
 })
 
 export class UserProfilePopoverComponent implements OnInit {
@@ -17,24 +17,21 @@ export class UserProfilePopoverComponent implements OnInit {
   @Output() avatarSelected = new EventEmitter<string>();
   selectedAvatar: string | null = null;
 
-  constructor(private popoverCtrl: PopoverController) {}
+  constructor(private popoverCtrl: PopoverController, private navCtrl: NavController) {}
 
-  ngOnInit() {
-      // get actual pic from database
-      this.defaultAvatars = [
-      'assets/avatars/avatar1.png',
-      'assets/avatars/avatar2.png',
-      'assets/avatars/avatar3.png',
-      'https://ionicframework.com/docs/img/demos/avatar.svg'
-    ];
-  }
+  ngOnInit() {}
 
-  // select avatar from popover
-  selectAvatar(avatarPath: string) {
-    this.popoverCtrl.dismiss(avatarPath);
+  // // select avatar from popover
+  // selectAvatar(avatarPath: string) {
+  //   this.popoverCtrl.dismiss(avatarPath);
+  // }
 
-    // this.selectedAvatar = avatarPath;
-    // this.avatarSelected.emit(avatarPath);
+  // logout
+  async logout() {
+    
+    await this.popoverCtrl.dismiss();
+
+    this.navCtrl.navigateRoot('/login');
   }
 
 }
