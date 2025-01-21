72b5fdf4471308aedb58b4462fb7c612c75e436f
Rui En Koe <ruienkoe@gmail.com>
2025-01-21 12:23:19 +0800

    update loader test bed and router for loader and user profile page

--- src/app/pages/loader/loader.page.spec.ts
+++ src/app/pages/loader/loader.page.spec.ts
@@ -1,17 +1,42 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { LoaderPage } from './loader.page';
+import { IonicModule } from '@ionic/angular';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { Router } from '@angular/router';
 
 describe('LoaderPage', () => {
   let component: LoaderPage;
   let fixture: ComponentFixture<LoaderPage>;
+  let router: Router;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [LoaderPage],
+      imports: [
+        IonicModule.forRoot(),
+        AppRoutingModule
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(LoaderPage);
+    router = TestBed.get(Router);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    
   });
+
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should go to login page after load', fakeAsync(() => {
+    spyOn(router, 'navigate');
+
+    component.ngOnInit();
+
+    tick(2000);
+
+    expect(router.navigate).toHaveBeenCalledWith(['login']);
+  }))
+  
 });
--- src/app/pages/loader/loader.page.ts
+++ src/app/pages/loader/loader.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-loader',
@@ -8,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoaderPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
     
+   // Navigate to home page after animation
+   setTimeout(() => {
+    //navigation logic 
+    this.router.navigate(['login'])
+  }, 2000);
+
   }
 
 }
