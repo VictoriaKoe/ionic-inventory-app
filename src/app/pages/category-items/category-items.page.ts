a388ec011b71d28bbb737359c218b9919789b6dc
Rui En Koe <ruienkoe@gmail.com>
2025-02-03 10:54:20 +0800

    render items on category page, need to update data from real database

--- src/app/pages/category-items/category-items.page.ts
+++ src/app/pages/category-items/category-items.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-category-items',
@@ -8,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryItemsPage implements OnInit {
 
-  constructor() { }
+  itemData: any[] = [];
+  categoryName!:string;
+
+  constructor(private navCtrl: NavController) { }
 
   ngOnInit() {
+
+    // dummy data
+
+    this.itemData = [
+      {
+        itemId: 1,
+        item_name: 'Lightstick',
+        cat_name: 'Official KPOP Merchandise',
+        item_status: ['Wear']
+      },
+
+      {
+        itemId: 2,
+        item_name: 'Photocard',
+        cat_name: 'Official KPOP Merchandise',
+        item_status: ['New', 'Ordered']
+      }
+
+    ];
+
+    this.categoryName = this.itemData[0].cat_name;
+
+    // retrive data from db
+
+
   }
+
+  // view item details
+  viewItemDetails(item: any){
+  // todo: save specific item details to db to render on item details page
+    
+  this.navCtrl.navigateForward('item-details');
+}
 
 }
