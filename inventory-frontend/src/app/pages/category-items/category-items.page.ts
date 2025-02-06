0b0bf667bef1e2b73ecd98b4a8dad06161dad0a0
Rui En Koe <ruienkoe@gmail.com>
2025-02-06 11:25:26 +0800

    update image property to further render data dynamically from db

--- inventory-frontend/src/app/pages/category-items/category-items.page.ts
+++ inventory-frontend/src/app/pages/category-items/category-items.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { ItemCategoriesService } from 'src/app/services/item-categories.service';
 
 @Component({
   selector: 'app-category-items',
@@ -11,42 +12,50 @@ export class CategoryItemsPage implements OnInit {
 
   itemData: any[] = [];
   categoryName!:string;
+  // default val
+  item_image: any = "https://ionicframework.com/docs/img/demos/card-media.png"
 
-  constructor(private navCtrl: NavController) { }
+  constructor(
+    private navCtrl: NavController,
+    private categoryData: ItemCategoriesService 
+  ) {}
 
   ngOnInit() {
 
     // dummy data
-
     this.itemData = [
       {
         itemId: 1,
         item_name: 'Lightstick',
         cat_name: 'Official KPOP Merchandise',
-        item_status: ['Wear']
+        item_status: ['Wear'],
+        item_image: "assets/item/army_bomb.jpg"
       },
 
       {
         itemId: 2,
         item_name: 'Photocard',
         cat_name: 'Official KPOP Merchandise',
-        item_status: ['New', 'Ordered']
+        item_status: ['New', 'Ordered'],
+        item_image: "assets/item/photocard1.jpg"
       }
 
     ];
 
-    this.categoryName = this.itemData[0].cat_name;
+    // todo: retrive item data of specfic category from db / other services
 
-    // retrive data from db
+    // category name
+    this.categoryName = this.categoryData.getCategoryName();
 
+    // category item data 
 
   }
 
   // view item details
   viewItemDetails(item: any){
-  // todo: save specific item details to db to render on item details page
+    // todo: save specific item details to db to render on item details page
     
-  this.navCtrl.navigateForward('item-details');
-}
+    this.navCtrl.navigateForward('item-details');
+  }
 
 }
