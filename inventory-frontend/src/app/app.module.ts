cf77f9da3ffbf226f6008bf595e681f3cc5c1bad
Rui En Koe <ruienkoe@gmail.com>
2025-02-10 17:58:24 +0800

    update project environment

--- inventory-frontend/src/app/app.module.ts
+++ inventory-frontend/src/app/app.module.ts
@@ -32,11 +32,8 @@ import { environment } from 'src/environments/environment';
 
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
-    // provideFirebaseApp(() => initializeApp(
-    //   { projectId: "inventory-ionic-project-80004", 
-    //     appId: "1:1097472434579:web:fe7f31a311b43612648866", 
-    //     databaseURL: "https://inventory-ionic-project-80004-default-rtdb.asia-southeast1.firebasedatabase.app", storageBucket: "inventory-ionic-project-80004.firebasestorage.app", apiKey: "AIzaSyA2I_Ym9s3XMKFB0W7270w4sIZ1cLFXZnE", authDomain: "inventory-ionic-project-80004.firebaseapp.com", messagingSenderId: "1097472434579" })),
-    // provideAuth(() => getAuth()),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
     provideAnalytics(() => getAnalytics()),
     ScreenTrackingService,
     UserTrackingService,
--- inventory-frontend/src/environments/environment.prod.ts
+++ inventory-frontend/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyA2I_Ym9s3XMKFB0W7270w4sIZ1cLFXZnE",
+    authDomain: "inventory-ionic-project-80004.firebaseapp.com",
+    databaseURL: "https://inventory-ionic-project-80004-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "inventory-ionic-project-80004",
+    storageBucket: "inventory-ionic-project-80004.firebasestorage.app",
+    messagingSenderId: "1097472434579",
+    appId: "1:1097472434579:web:fe7f31a311b43612648866"
+  }
 };
